2a0ddd8e56c530d791e91df971a133c2c469b065
Tim Zulf <timzulf@gmail.com>
2014-11-01 01:29:15 -0400

    cleaning up

---
+++ thisjs.js
@@ -0,0 +1,5 @@
+//make sure user connects to a room//
+window.location.replace("http://www.domain.com/home.html");
+
+
+var postsRef = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts")
