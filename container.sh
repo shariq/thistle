8dc01ef0814434d14df3351354f38bf804b4141f
root <root@thistle.io>
2014-12-13 07:29:35 -0500

    container.sh worked; so stuck it into acorn

--- container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This shell script will spin up a new Docker container running room.py
-
-docker run -d --name $1 -p 127.0.0.1:$1:6200 thistle $1
-#docker run -d --name $1 thistle $1
-
