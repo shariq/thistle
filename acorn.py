9395f69fd7755ff0423ae068b341b84d2d79f7cc
root <root@thistle.io>
2014-12-13 07:28:39 -0500

    split off docker handlers into acorn module; had monkey patch incompatibility

---
+++ acorn.py
@@ -0,0 +1,95 @@
+# This Python script is imported as a module and can spin up Docker
+# containers, pass messages to them, and pass messages received from
+# them to a handler function.
+
+import time
+import traceback
+from threading import Thread
+
+from multiprocessing.managers import BaseManager
+from multiprocessing import Queue
+import random
+import os
+
+def printf(s):
+    print s
+
+class MessagePasser:
+    def __init__(self, port, function_receive=printf, ip_address='127.0.0.1'):
+        class QueueManager(BaseManager):pass
+        QueueManager.register('getInputQueue')
+        QueueManager.register('getOutputQueue')
+        self.qm = QueueManager(address=(ip_address,port), authkey='magic')
+        while True:
+            try: # waits a long time for connection
+                self.qm.connect()
+                break
+            except:
+                print 'COULD NOT CONNECT TO REMOTE MANAGER'
+                print traceback.format_exc()
+                time.sleep(5)
+        self.i = self.qm.getInputQueue()
+        self.o = self.qm.getOutputQueue()
+        def infiniteReceive(Q, f):
+            while True:
+                try:
+                    e = Q.get()
+                    if e is None:return
+                    f(e)
+                except:
+                    print traceback.format_exc()
+                    time.sleep(0.25)
+        self.receiver = Thread(target = infiniteReceive, args = (self.o, function_receive))
+        self.receiver.start()
+    def send(self, m):
+        self.i.put(m)
+    def stop(self):
+        self.i.put(None)
+        self.o.put(None)
+        self.receiver.join()
+
+ports_used = set()
+name_port_dictionary = {}
+name_passer_dictionary = {}
+
+def makeDocker(room_name, function_receive = printf):
+    if room_name in name_port_dictionary:
+        return
+    try:
+        function_receive('Spinning up new Docker instance...')
+    except:
+        print traceback.format_exc()
+        return
+    attempts = 0
+    while True:
+        attempts += 1
+        if attempts > 15:
+            print 'YIKES! TRIED TO MAKEDOCKER 15 TIMES. DECIDED TO STOP RETRYING.'
+            break
+        port = random.randint(40000,50000)
+        if port in ports_used:
+            continue
+        error = os.system('docker run -d --name $1 -p 127.0.0.1:$1:6200 thistle $1'.replace('$1',str(port)))
+        if error:
+            continue
+        ports_used.add(port)
+        name_port_dictionary[room_name] = port
+        name_passer_dictionary[room_name] = MessagePasser(port, function_receive)
+        break
+
+def sendDocker(room_name, message = 'test'):
+    name_passer_dictionary[room_name].send(message)
+
+def breakDocker(room_name):
+    if room_name not in name_passer_dictionary or room_name not in name_port_dictionary:
+        print 'Tried to breakDocker '+room_name+' but couldn\'t find it...'
+        return
+    try:
+        name_passer_dictionary[room_name].stop()
+        port = name_port_dictionary[room_name]
+        os.system('docker kill '+str(port)) # ain't nobody have time for docker stop
+        ports_used.remove(port)
+        del name_passer_dictionary[room_name]
+        del name_port_dictionary[room_name]
+    except:
+        print traceback.format_exc()
--- app.py
+++ app.py
@@ -1,142 +1,45 @@
 # This Python script will be in charge of delivering static content,
-# spinning up Docker containers, passing messages from clients to the
-# Docker instances, passing messages from the Docker containers to
-# clients, and spinning down Docker containers.
+# and will use acorn to manage Docker containers. The script will
+# also handle all the socket stuff.
 
 # It uses Flask and gevent; but I'm not sure if this is meant to be
 # deployed. I'll be deploying it anyways.
 
 # started with https://github.com/miguelgrinberg/Flask-SocketIO/tree/master/example
 
-from gevent import monkey
 
-monkey.patch_all(socket = False, thread = False)
-# can't find better way to fix issues with gevent...
-# I imagine this will significantly slow down server in prod
-
-import time
-import traceback
-from threading import Thread
 from flask import Flask, session, request
 from flask.ext.socketio import SocketIO, emit, join_room, leave_room
 
-from multiprocessing.managers import BaseManager
-from multiprocessing import Queue
-import random
-import os
-
-def printf(s):
-    print s
+from acorn import makeDocker, sendDocker, breakDocker
+# makeDocker(room_name, on_receive_handler) : if room_name already exists, returns without error
+# sendDocker(room_name, message) : message is input for the Docker container
+# breakDocker(room_name) : prints weird stuff if room_name does not exist; runs docker kill
 
 # MIGHT NOT WANT TO DO THIS IN OTHER CASES!
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
+room_html = open('room.html').read()
 
-class MessagePasser:
-    def __init__(self, port, function_receive=printf, ip_address='127.0.0.1'):
-        class QueueManager(BaseManager):pass
-        QueueManager.register('getInputQueue')
-        QueueManager.register('getOutputQueue')
-        self.qm = QueueManager(address=(ip_address,port), authkey='magic')
-        while True:
-            try: # waits a long time for connection
-                self.qm.connect()
-                break
-            except:
-                print 'COULD NOT CONNECT TO REMOTE MANAGER'
-                print traceback.format_exc()
-                time.sleep(5)
-        self.i = self.qm.getInputQueue()
-        self.o = self.qm.getOutputQueue()
-        def infiniteReceive(Q, f):
-            while True:
-                try:
-                    e = Q.get()
-                    if e is None:return
-                    f(e)
-                except:
-                    pass
-        self.receiver = Thread(target = infiniteReceive, args = (self.o, function_receive))
-        self.receiver.start()
-    def send(self, m):
-        self.i.put(m)
-    def stop(self):
-        self.i.put(None)
-        self.o.put(None)
-        self.receiver.join()
-
-ports_used = set()
-name_port_dictionary = {}
-name_passer_dictionary = {}
-
-def makeDocker(room_name, function_receive = printf):
-    port = [random.randint(40000,50000)]
-    while port[0] in ports_used:
-        port[0] = random.randint(40000,50000)
-    port = port[0]
-    ports_used.add(port)
-    name_port_dictionary[room_name] = port
-    os.system('./container.sh '+str(port))
-    name_passer_dictionary[room_name] = MessagePasser(port, function_receive)
-
-def sendDocker(room_name, message):
-    name_passer_dictionary[room_name].send(message)
-
-def breakDocker(room_name):
-    name_passer_dictionary[room_name].stop()
-    port = name_port_dictionary[room_name]
-    os.system('docker kill '+str(port))#or docker stop :p
-    ports_used.remove(port)
-    del name_passer_dictionary[room_name]
-    del name_port_dictionary[room_name]
 
 
 app = Flask(__name__)
 app.debug = True # remove this in prod!
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
-thread = None
-
-room_html = open('room.html').read()
-
-def background_thread():
-    """Example of how to send server generated events to clients."""
-    count = 0
-    while True:
-        time.sleep(10)
-        count += 1
-        socketio.emit('my response',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='/socket')
 
 @app.route('/')
 def index():
-    global thread
-    if thread is None:
-        thread = Thread(target=background_thread)
-        thread.start()
     return room_html
-
-
-@socketio.on('my event', namespace='/socket')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': message['data'], 'count': session['receive_count']})
-
-
-@socketio.on('my broadcast event', namespace='/socket')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
 
 @socketio.on('join', namespace='/socket')
 def join(message):
-    join_room(message['room'])
+    # This should be the part of the URL after /
+    roomName = message['room']
+    join_room(roomName)
+    # Does not makeDocker if it already exists
+    makeDocker(roomName, eval('lambda x:socketio.emit("my response", {"data":x},room='+roomName+')'))
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my response',
          {'data': 'In rooms: ' + ', '.join(request.namespace.rooms),
@@ -145,6 +48,7 @@ def join(message):
 
 @socketio.on('leave', namespace='/socket')
 def leave(message):
+    # check if room is empty, if so, breakDocker
     leave_room(message['room'])
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my response',
@@ -155,11 +59,11 @@ def leave(message):
 @socketio.on('my room event', namespace='/socket')
 def send_room_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
+    sendDocker(message['room'], message['data']);
 
 
+'''
+# no need to worry about connect and disconnect if they're equivalent to joining and leaving rooms
 @socketio.on('connect', namespace='/socket')
 def test_connect():
     emit('my response', {'data': 'Connected', 'count': 0})
@@ -168,7 +72,12 @@ def test_connect():
 @socketio.on('disconnect', namespace='/socket')
 def test_disconnect():
     print('Client disconnected')
+'''
 
 
 if __name__ == '__main__':
+# can't find better way to fix issues with gevent...
+# I imagine this will significantly slow down server in prod
+#    from gevent import monkey
+#    monkey.patch_all()
     socketio.run(app, host = '0.0.0.0', port = 80)
